8bca8b0ee7ae99c54f4a8b4c55e28f0fc2b14072
Dmytro Farbun <dimofarb@gmail.com>
2026-03-05 22:18:46 +0200

    fix: profile layout.tsx

--- app/(main)/(private routes)/profile/layout.tsx
+++ app/(main)/(private routes)/profile/layout.tsx
@@ -1,24 +1,12 @@
 import type { Metadata } from 'next';
+import { redirect } from 'next/navigation';
 import { getMeServer } from '@/lib/api/serverApi';
 import TravellerInfo from '@/components/OurTravellers/TravellerInfo';
 import type { ReactNode } from 'react';
 import PageToggle from '@/components/PageToggle/PageToggle';
+
 export const metadata: Metadata = {
-  title: 'Profile Page',
-  description: 'My profile',
-  openGraph: {
-    title: 'Profile Page',
-    description: 'My profile',
-    url: 'https://08-zustand-valeriia-makushchenko.vercel.app/',
-    images: [
-      {
-        url: 'https://ac.goit.global/fullstack/react/og-meta.jpg',
-        width: 1200,
-        height: 630,
-        alt: 'Note Hub',
-      },
-    ],
-  },
+  title: 'Profile',
 };
 
 export default async function ProfileLayout({
@@ -27,7 +15,10 @@ export default async function ProfileLayout({
   children: ReactNode;
 }) {
   const user = await getMeServer();
-  // if (!user) redirect('/login');
+
+  if (!user) {
+    redirect('/login');
+  }
 
   return (
     <section>
